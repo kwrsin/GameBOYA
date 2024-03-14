8f1da00df1b120bda1d25b5d52a3aacd16063fad
kwrsin <kwrsin@gmail.com>
2024-03-15 08:38:12 +0900

    deepcopy

--- src/libs/bindings/publisher.lua
+++ src/libs/bindings/publisher.lua
@@ -4,10 +4,26 @@ local subscribers = {}
 
 local M = {}
 
+local function deepcopy(orig)
+  local orig_type = type(orig)
+  local copy
+  if orig_type == 'table' then
+      copy = {}
+      for orig_key, orig_value in next, orig, nil do
+          copy[deepcopy(orig_key)] = deepcopy(orig_value)
+      end
+      setmetatable(copy, deepcopy(getmetatable(orig)))
+  else -- number, string, boolean, etc
+      copy = orig
+  end
+  return copy
+end
+
 local function merge(params, options)
 	if params == nil then
 		return options
 	end
+	local params = deepcopy(params)	
 	for k, v in pairs(params) do
 		options[k] = params[k]		
 	end
--- src/libs/utils.lua
+++ src/libs/utils.lua
@@ -4,10 +4,27 @@ local network = require 'network'
 local composer = require 'composer'
 
 local M = {}
+
+local function deepcopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[deepcopy(orig_key)] = deepcopy(orig_value)
+        end
+        setmetatable(copy, deepcopy(getmetatable(orig)))
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
+
 local function merge(params, options)
 	if params == nil then
 		return options
 	end
+	local params = deepcopy(params)
 	for k, v in pairs(params) do
 		options[k] = params[k]		
 	end
@@ -111,21 +128,6 @@ end
 
 local function removeScene(name)
 	composer.removeScene( dotPath(name, dot_scenes) )
-end
-
-local function deepcopy(orig)
-    local orig_type = type(orig)
-    local copy
-    if orig_type == 'table' then
-        copy = {}
-        for orig_key, orig_value in next, orig, nil do
-            copy[deepcopy(orig_key)] = deepcopy(orig_value)
-        end
-        setmetatable(copy, deepcopy(getmetatable(orig)))
-    else -- number, string, boolean, etc
-        copy = orig
-    end
-    return copy
 end
 
 M.dotPath = dotPath
