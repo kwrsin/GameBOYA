1fe4bf982a5e207fac60a0ac629fb586dda44a01
kwrsin <kwrsin@gmail.com>
2024-04-22 16:40:16 +0900

    tap event

--- src/scenes/contents/contentManager.lua
+++ src/scenes/contents/contentManager.lua
@@ -1,16 +1,24 @@
 -- contentManager.lua
 local M = require('src.scenes.contents.base')()
 
+local function tap(event)
+  contentManager:result{message="Start Game!!"}
+end
+
 function M:result(params)
-  storage:put('selectedLevel', utils.dotPath('levels.level01', DOT_STRUCTURES))
+  storage:put('selectedLevel', utils.dotPath('levels.opening', DOT_STRUCTURES))
   storage:store()
   player = nil
-  self:gotoNext({params={message=params.message}})
+  self:gotoNext({params={message=params.message or "Next..."}})
 end
 
 function M:start()
-  logger.info('START GAME')
-  sound:music1('bgm', {loops=-1})
+  logger.info('START TITLE')
+  Runtime:addEventListener( 'tap', tap)
+end
+
+function M:destroy()
+  Runtime:removeEventListener( 'tap', tap)
 end
 
 return M
