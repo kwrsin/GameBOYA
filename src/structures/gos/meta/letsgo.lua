5d88cdbc95248ce9edb15ac24c184af5f25d65a2
kwrsin <kwrsin@gmail.com>
2024-04-22 22:51:36 +0900

    Create letsgo.lua

---
+++ src/structures/gos/meta/letsgo.lua
@@ -0,0 +1,12 @@
+return {
+ frameNum = 1,
+ path = "src.structures.gos.letsgo",
+ class = "letsgo",
+ props = {
+  yScale = 1,
+  xScale = 1,
+  rotation = 0,
+  anchorX = 0.5,
+  anchorY = 0.5,
+ },
+}
